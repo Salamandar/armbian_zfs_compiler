5c5cca3187118a4869ae82914749475577071963
Votre Nom <Vous@exemple.com>
2020-12-01 09:56:04 +0100

    fixes

--- build_all.sh
+++ build_all.sh
@@ -5,9 +5,9 @@ zfs_version=2.0.0-rc6
 
 
 # Get ZFS source code
-sudo apt install --no-install-recommends --yes git
+if ! command -v git;then sudo apt install --no-install-recommends --yes git; fi
 if [[ ! -d zfs ]]; then git clone https://github.com/openzfs/zfs.git; else git -C zfs fetch; fi
-git -C zfs clean -xdf
+sudo git -C zfs clean -xdf
 git -C zfs checkout "zfs-${zfs_version}"
 
 # Get linux kernel headers
@@ -23,10 +23,10 @@ pushd utils_builder
 popd
 
 
-git -C zfs clean -xdf
 docker run -it --rm -v $(pwd):/build_zfs zfs_builder/module /build_zfs/build_in_docker.sh
 sudo mv zfs/*.deb module_builder
+sudo git -C zfs clean -xdf
 
-git -C zfs clean -xdf
 docker run -it --rm -v $(pwd):/build_zfs zfs_builder/utils /build_zfs/build_in_docker.sh
 sudo mv zfs/*.deb utils_builder
+sudo git -C zfs clean -xdf
--- build_in_docker.sh
+++ build_in_docker.sh
@@ -1,25 +1,24 @@
 #!/usr/bin/env bash
 set -eEux
 
-kernel_version=legacy
+header_pkg_version=legacy
+header_version=$(uname -r | sed 's/rk3399/rockchip64/')
 
 cd /build_zfs
-dpkg -i linux-headers-${kernel_version}-*.deb || true
-sed -i '/+= selinux/s/^/# /' /usr/src/linux-headers-*-rockchip64/scripts/Makefile
-dpkg-reconfigure "linux-headers-${kernel_version}-rockchip64"
+dpkg -i linux-headers-${header_pkg_version}-*.deb || true
+sed -i '/+= selinux/s/^/# /' "/usr/src/linux-headers-${header_version}/scripts/Makefile"
+dpkg-reconfigure "linux-headers-${header_pkg_version}-rockchip64"
 
 # Disable all STACKPROTECT options incompatible with GCC, this means the
 # built kernel module (kmod) will be non-functional. That's OK since
 # we're only interested in building the tools on Armbian.
 if grep buster /etc/os-release; then
-    sed -i -e 's/\(.*STACKPROTECT.*=\)y/\1n/' /usr/src/linux-headers-$(uname -r)/.config
+    sed -i -e 's/\(.*STACKPROTECT.*=\)y/\1n/' "/usr/src/linux-headers-${header_version}/.config"
 fi
 
 pushd zfs
     sh autogen.sh
-    ./configure
+    ./configure --with-linux="/usr/src/linux-headers-${header_version}"
     make -s -j$(nproc)
     make -s -j$(nproc) deb
 popd
-
-mv zfs/*.deb module_builder
